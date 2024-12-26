2f246a3797dff56cdb99afd30a53dd8d177eb0f0
Oscar Mumm <oscarmumm@gmail.com>
2024-12-26 16:42:53 -0300

    styling of filter elements on sale histoy view

--- src/pages/SalesHistory.jsx
+++ src/pages/SalesHistory.jsx
@@ -1,10 +1,10 @@
-import {useContext, useEffect, useState} from 'react'
-import {DataContext} from '../context/DataContext'
-import {SaleHistoryModal} from '../components/Modals/SaleHistoryModal'
-import {AnimatePresence} from 'framer-motion'
+import { useContext, useEffect, useState } from 'react'
+import { DataContext } from '../context/DataContext'
+import { SaleHistoryModal } from '../components/Modals/SaleHistoryModal'
+import { AnimatePresence } from 'framer-motion'
 
 export const SalesHistory = () => {
-    const {data, setData} = useContext(DataContext)
+    const { data, setData } = useContext(DataContext)
     const [ventas, setVentas] = useState(data.ventas)
     const [saleHistoryModalActive, setSaleHistoryModalActive] = useState(false)
     const [selectedSale, setSelectedSale] = useState()
@@ -56,10 +56,10 @@ export const SalesHistory = () => {
             )
             setFilteredSales(filteredData)
         }
-        if(startDate === '' && endDate === '' && paymentMethod === '') {
+        if (startDate === '' && endDate === '' && paymentMethod === '') {
             setFilteredSales(ventas)
         }
-    }   
+    }
 
     const quitFilters = () => {
         setStartDate('')
@@ -70,95 +70,106 @@ export const SalesHistory = () => {
 
     return (
         <div
-            className="flex flex-col items-center h-full overflow-auto p-3"
-            style={{maxHeight: 'calc(100vh - 64px)'}}>
-            <h2 className="text-2xl my-6">Historial de Ventas</h2>
-            <span className=" text-xl my-3">Filtros:</span>
-            <div className="flex items-end mb-5 p-3 rounded-md shadow-lg bg-slate-500 text-slate-50 min-w-max">
-                <div className="flex flex-col">
-                    <label>Desde</label>
-                    <input
-                        className="p-2 rounded-md shadow-lg text-slate-900 outline-none"
-                        type="date"
-                        value={startDate}
-                        onChange={(e) => setStartDate(e.target.value)}
-                    />
+            className='flex flex-col items-center h-full overflow-auto p-3'
+            style={{ maxHeight: 'calc(100vh - 64px)' }}
+        >
+            <h2 className='text-2xl my-6'>Historial de Ventas</h2>
+            <div className='p-3 flex justify-center min-w-max w-full'>
+                <div className='flex flex-col'>
+                    <span className=' text-xl my-3'>Filtros:</span>
+                    <div className='flex flex-col items-start mb-5 p-3 rounded-md shadow-lg bg-slate-500 text-slate-50 min-w-max'>
+                        <label>Desde</label>
+                        <input
+                            className='p-2 rounded-md shadow-lg text-slate-900 outline-none'
+                            type='date'
+                            value={startDate}
+                            onChange={(e) => setStartDate(e.target.value)}
+                        />
+                        <label>Hasta</label>
+                        <input
+                            className='p-2 rounded-md shadow-lg text-slate-900 outline-none'
+                            type='date'
+                            value={endDate}
+                            onChange={(e) => setEndDate(e.target.value)}
+                        />
+                        <label>Método de pago</label>
+                        <select
+                            name='paymentMethod'
+                            className='p-3 outline-none rounded-md shadow-lg text-slate-900'
+                            value={paymentMethod}
+                            onChange={selectMethod}
+                        >
+                            <option value=''>Elije un método de pago</option>
+                            {paymentMethodOptions.map((option, index) => (
+                                <option
+                                    key={index}
+                                    value={option}
+                                    onChange={(e) =>
+                                        setPaymentMethod(e.target.value)
+                                    }
+                                >
+                                    {option}
+                                </option>
+                            ))}
+                        </select>
+                        <button
+                            className='bg-emerald-500 hover:bg-emerald-400 text-slate-50 font-bold mt-3 p-2 w-24 rounded-md shadow-lg'
+                            onClick={applyFilters}
+                        >
+                            Aplicar
+                        </button>
+                        <button
+                            className='bg-yellow-500 hover:bg-yellow-400 text-slate-50 font-bold mt-3 p-2 w-32 rounded-md shadow-lg'
+                            onClick={quitFilters}
+                        >
+                            Quitar filtros
+                        </button>
+                    </div>
                 </div>
-                <div className="flex flex-col ml-3">
-                    <label>Hasta</label>
-                    <input
-                        className="p-2 rounded-md shadow-lg text-slate-900 outline-none"
-                        type="date"
-                        value={endDate}
-                        onChange={(e) => setEndDate(e.target.value)}
-                    />
+                <div className='p-3 felx flex-col'>
+                    <table className=' bg-slate-50 text-center min-w-max max-w-screen-md'>
+                        <thead className='bg-slate-500 text-slate-200'>
+                            <tr>
+                                <th className='p-3 min-w-40'>Fecha</th>
+                                <th className='p-3 min-w-40'>Hora</th>
+                                <th className='p-3 min-w-40'>Monto</th>
+                                <th className='p-3 min-w-40'>Método de Pago</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {filteredSales.map((sale) => (
+                                <tr
+                                    key={sale.id}
+                                    className='hover:bg-slate-200 border-t-slate-200 border-t-2 cursor-pointer'
+                                    onClick={() => {
+                                        setSelectedSale(sale)
+                                        setSaleHistoryModalActive(true)
+                                    }}
+                                >
+                                    <td className='p-3'>{sale.fecha}</td>
+                                    <td className='p-3'>{sale.hora}</td>
+                                    <td className='p-3'>${sale.importe}</td>
+                                    <td className='p-3'>{sale.metodoDePago}</td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+
+                    {filteredSales.length === 0 && (
+                        <p className='text-xl text-center p-3'>
+                            No hay resultados para la búsqueda solicitada
+                        </p>
+                    )}
                 </div>
-                <div className="flex flex-col ml-3">
-                    <label>Método de pago</label>
-                    <select
-                        name="paymentMethod"
-                        className="p-3 outline-none rounded-md shadow-lg text-slate-900"
-                        value={paymentMethod}
-                        onChange={selectMethod}>
-                        <option value="">Elije un método de pago</option>
-                        {paymentMethodOptions.map((option, index) => (
-                            <option
-                                key={index}
-                                value={option}
-                                onChange={(e) =>
-                                    setPaymentMethod(e.target.value)
-                                }>
-                                {option}
-                            </option>
-                        ))}
-                    </select>
-                </div>
-                <button
-                    className="bg-emerald-500 hover:bg-emerald-400 text-slate-50 font-bold p-2 ml-3 w-24 rounded-md shadow-lg"
-                    onClick={applyFilters}>
-                    Aplicar
-                </button>
-                <button
-                    className="bg-yellow-500 hover:bg-yellow-400 text-slate-50 font-bold p-2 ml-3 w-32 rounded-md shadow-lg"
-                    onClick={quitFilters}>
-                    Quitar filtros
-                </button>
+                <AnimatePresence>
+                    {saleHistoryModalActive && (
+                        <SaleHistoryModal
+                            sale={selectedSale}
+                            closeSaleHistoryModal={closeSaleHistoryModal}
+                        />
+                    )}
+                </AnimatePresence>
             </div>
-            <table className="bg-slate-50 text-center min-w-max max-w-screen-md">
-                <thead className="bg-slate-500 text-slate-200">
-                    <tr>
-                        <th className="p-3 min-w-40">Fecha</th>
-                        <th className="p-3 min-w-40">Hora</th>
-                        <th className="p-3 min-w-40">Monto</th>
-                        <th className="p-3 min-w-40">Método de Pago</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {filteredSales.map((sale) => (
-                        <tr
-                            key={sale.id}
-                            className="hover:bg-slate-200 border-t-slate-200 border-t-2 cursor-pointer"
-                            onClick={() => {
-                                setSelectedSale(sale)
-                                setSaleHistoryModalActive(true)
-                            }}>
-                            <td className="p-3">{sale.fecha}</td>
-                            <td className="p-3">{sale.hora}</td>
-                            <td className="p-3">${sale.importe}</td>
-                            <td className="p-3">{sale.metodoDePago}</td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
-                    {filteredSales.length === 0 && <p className='text-xl p-3'>No hay resultados para la búsqueda solicitada</p>}
-            <AnimatePresence>
-                {saleHistoryModalActive && (
-                    <SaleHistoryModal
-                        sale={selectedSale}
-                        closeSaleHistoryModal={closeSaleHistoryModal}
-                    />
-                )}
-            </AnimatePresence>
         </div>
     )
 }
