4a932bbeded3d953797281ea7ee75f871f6d0e54
Oscar Mumm <oscarmumm@gmail.com>
2025-01-09 13:41:19 -0300

    password view on click modified on login component

--- src/pages/LogIn.jsx
+++ src/pages/LogIn.jsx
@@ -1,16 +1,19 @@
-import { UserContext } from '../context/UserContext'
-import { useState, useContext } from 'react'
-import { DataContext } from '../context/DataContext'
+import {UserContext} from '../context/UserContext'
+import {useState, useContext} from 'react'
+import {DataContext} from '../context/DataContext'
+import {MdOutlineVisibility} from 'react-icons/md'
+import {IconContext} from 'react-icons'
 
 export const LogIn = () => {
-    const { user, logIn, logOut } = useContext(UserContext)
-    const { data, setData } = useContext(DataContext)
+    const {user, logIn, logOut} = useContext(UserContext)
+    const {data, setData} = useContext(DataContext)
     const [userCredentials, setUserCredentials] = useState({
         username: '',
         password: '',
     })
     const [forgottenPassword, setForgottenPassword] = useState(false)
     const [errorMessage, setErrorMessage] = useState()
+    const [viewPassInputValueActive, setViewPassInputValueActive] = useState(false)
 
     const handleChange = (e) => {
         setUserCredentials({
@@ -25,7 +28,7 @@ export const LogIn = () => {
             (usuario) => usuario.nombreUsuario === userCredentials.username
         )
         if (auth && auth.password === userCredentials.password) {
-            logIn({ username: auth.nombreUsuario, role: auth.role })
+            logIn({username: auth.nombreUsuario, role: auth.role})
         } else {
             setErrorMessage('Datos de inicio de sesión incorrectos')
         }
@@ -36,49 +39,60 @@ export const LogIn = () => {
         logOut()
     }
 
+    const viewPasswordInputValue = (e) => {
+        e.preventDefault()
+        setViewPassInputValueActive(!viewPassInputValueActive)
+    }
+
     return (
-        <div className='flex flex-col items-center justify-center h-screen w-screen overflow-auto bg-slate-300 text-slate-800'>
-            <h1 className='text-3xl font-semibold  mb-10'>
+        <div className="flex flex-col items-center justify-center h-screen w-screen overflow-auto bg-slate-300 text-slate-800">
+            <h1 className="text-3xl font-semibold  mb-10">
                 Sistema de Gestión de Comercio
             </h1>
-            <div className='bg-slate-100 py-5 px-10 w-fit rounded-xl min-w-96 shadow-xl'>
-                <p className='text-center text-xl my-5'>Iniciar Sesión</p>
-                <form className='flex flex-col'>
-                    <label className='mb-1' htmlFor=''>
+            <div className="bg-slate-100 py-5 px-10 w-fit rounded-xl min-w-96 shadow-xl">
+                <p className="text-center text-xl my-5">Iniciar Sesión</p>
+                <form className="flex flex-col">
+                    <label className="mb-1" htmlFor="">
                         Usuario
                     </label>
                     <input
-                        className='mb-3 p-2 rounded-md outline-none shadow-lg'
-                        type='text'
-                        name='username'
+                        className="mb-3 p-2 rounded-md outline-none shadow-lg relative"
+                        type="text"
+                        name="username"
                         onChange={handleChange}
+                        autoComplete="off"
                     />
-                    <label className='mb-1' htmlFor=''>
+
+                    <label className="mb-1" htmlFor="">
                         Contraseña
                     </label>
-                    <input
-                        className='mb-3 p-2 rounded-md outline-none shadow-lg'
-                        type='text'
-                        name='password'
-                        onChange={handleChange}
-                    />
-                    <span className='w-full text-center my-3'>
-                        ¿Olvidó su contraseña?
-                    </span>
-                    <span className='w-full text-center text-red-600 font-semibold'>
+                    <div className="bg-white flex justify-between mb-3 p-2 rounded-md outline-none shadow-lg">
+                        <input
+                            className="outline-none"
+                            type={viewPassInputValueActive ? 'text' : 'password'}
+                            name="password"
+                            onChange={handleChange}
+                            autoComplete="off"
+                        />
+                        <button onClick={viewPasswordInputValue}>
+                            <IconContext.Provider value={{className: 'w-5 h-5'}}>
+                                <MdOutlineVisibility />
+                            </IconContext.Provider>
+                        </button>
+                    </div>
+                    <span className="w-full text-center text-red-600 font-semibold">
                         {errorMessage}
                     </span>
-                    <div className='flex justify-center mt-5'>
+                    <div className="flex justify-center mt-5">
                         <button
-                            className='bg-slate-700 hover:bg-slate-600 text-slate-50 p-2 w-48 ml-3 rounded-md shadow-lg'
-                            onClick={submitClick}
-                        >
+                            className="bg-slate-700 hover:bg-slate-600 text-slate-50 p-2 w-48 ml-3 rounded-md shadow-lg"
+                            onClick={submitClick}>
                             Iniciar Sesión
                         </button>
                     </div>
                 </form>
             </div>
-            <div className='mt-24'>
+            <div className="mt-24">
                 <p>
                     Si tiene problemas para iniciar sesión consulte al
                     administrador del sistema
